55934216ffcdb1cdebd817b058818edc0432c6b0
Daniela <ddquega@insa-rennes.fr>
2021-11-08 17:40:06 +0100

    Mejoramiento de las escenas

--- sampleProject/main.cpp
+++ sampleProject/main.cpp
@@ -27,6 +27,79 @@
 #include <iostream>
 using std::string;
 
+LightedMeshRenderablePtr* createSubmarine(Viewer& viewer, ShaderProgramPtr flatShader){
+    MaterialPtr silver = Material::Silver();
+    MaterialPtr pearl = Material::Pearl();
+    MaterialPtr emerald = Material::Emerald();
+    MaterialPtr bronze = Material::Bronze();
+    MaterialPtr turquoise = Material::Turquoise();
+    MaterialPtr whiteRubber = Material::WhiteRubber();
+    MaterialPtr yellowRubber = Material::YellowRubber();
+    MaterialPtr gold = Material::Gold();
+    MaterialPtr ruby = Material::Ruby();
+    MaterialPtr green = Material::Green();
+
+
+    LightedMeshRenderablePtr* subamrineGroup= new LightedMeshRenderablePtr[10];
+
+    // Cuerpo
+    subamrineGroup[0] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/cuerpo.obj");
+    subamrineGroup[0]->setMaterial(silver);
+
+    // Aletas
+     subamrineGroup[1] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/aletas.obj");
+    subamrineGroup[1]->setMaterial(ruby);
+
+    // Visor
+     subamrineGroup[2] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/visor.obj");
+    subamrineGroup[2]->setMaterial(bronze);
+
+    // Cola
+     subamrineGroup[3] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/cola.obj");
+    subamrineGroup[3]->setMaterial(yellowRubber);
+
+    // Ventana Lateral
+     subamrineGroup[4] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/ventana_laterales.obj");
+    subamrineGroup[4]->setMaterial(gold);
+
+    // Ventana Principal
+     subamrineGroup[5] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/ventana_principal.obj");
+    subamrineGroup[5]->setMaterial(gold);
+
+    // Ventana visor
+     subamrineGroup[6] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/ventana_visor.obj");
+    subamrineGroup[6]->setMaterial(gold);
+
+    // Vidrio Lateral
+     subamrineGroup[7] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/vidrio_laterales.obj");
+    subamrineGroup[7]->setMaterial(turquoise);
+
+    // Vidio Principal
+     subamrineGroup[8] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/vidrio_principal.obj");
+    subamrineGroup[8]->setMaterial(turquoise);
+
+    // Vidrio visor
+     subamrineGroup[9] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/vidrio_visor.obj");
+    subamrineGroup[9]->setMaterial(turquoise);
+    
+
+    // Children
+    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[1]);
+    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[2]);
+    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[3]);
+    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[4]);
+    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[5]);
+    HierarchicalRenderable :: addChild(subamrineGroup[2], subamrineGroup[6]);
+    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[7]);
+    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[8]);
+    HierarchicalRenderable :: addChild(subamrineGroup[2], subamrineGroup[9]);
+
+
+    return subamrineGroup;
+    //movingSubmarine( viewer, submarine);
+    
+}
+
 void movingSeahorse(float initialTime, 
                     Viewer& viewer, ShaderProgramPtr flatShader, 
                     glm::vec3 initial_pos, float duration
@@ -136,135 +209,83 @@ void Scene1 (Viewer& viewer, LightedMeshRenderablePtr submarine, LightedMeshRend
 }
 
 
-void Scene2 (Viewer& viewer, ShaderProgramPtr flatShader){
-        string caballito = "../../sfmlGraphicsPipeline/meshes/seahorse.obj";
-        glm::vec3 traslation = glm::vec3(30, 10,-0);
-        movingSeahorse(21, viewer, flatShader, glm::vec3(-55 , -40, -20 ) ,25);
-        movingSeahorse(21.1, viewer, flatShader, glm::vec3(-55 , -30, -10 ) ,25);
-        movingSeahorse(21.2, viewer, flatShader, glm::vec3(-60 , -40, -10 ) , 25);
-        movingSeahorse(21.3, viewer, flatShader, glm::vec3(-60 , -30, -20 ) ,25);
-        movingSeahorse(21.4, viewer, flatShader, glm::vec3(-65 , -40, -20 ) ,25);
-        movingSeahorse(21.5, viewer, flatShader, glm::vec3(-65 , -30, -10 ) ,25);
-        movingSeahorse(21.6, viewer, flatShader, glm::vec3(-70 , -40, -10 ) ,25);
-        movingSeahorse(21.7, viewer, flatShader, glm::vec3(-70 , -30, -20 ) ,25);
-        movingSeahorse(21.8, viewer, flatShader, glm::vec3(-75 , -40, -20 ) ,25);
-        movingSeahorse(21.9, viewer, flatShader, glm::vec3(-75 , -30, -10 ) ,25);
+void Scene2 (Viewer& viewer, ShaderProgramPtr flatShader, float initialTime, float duration){
+
+    float time = initialTime;
+    int pos_x = -55;
+    for (int i = 1; i < 6; i++)
+    {
+        if ( i %2 == 0){
+            movingSeahorse(time, viewer, flatShader, glm::vec3(pos_x + 8 , -45, -20 ) ,duration);
+            movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x + 2, -37, -15 ) ,duration);
+            movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x , -30, -10) ,duration);
+            movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x , -23, -5 ) ,duration);
+        }
+        else{
+            movingSeahorse(time, viewer, flatShader, glm::vec3(pos_x  , -45, -5 ) ,duration);
+            movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x , -37, -10 ) ,duration);
+            movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x + 2, -30, -15) ,duration);
+            movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x + 8 , -23, -20 ) ,duration);
+        }
+
+        pos_x-=5;
+        time+=0.2;
+    }
+    
 }
 
 void Scene3 (Viewer& viewer, ShaderProgramPtr flatShader,  
             LightedMeshRenderablePtr submarine, LightedMeshRenderablePtr aletas,
-            LightedMeshRenderablePtr visor ){
+            LightedMeshRenderablePtr visor,
+            float initialTime, float duration){
 
         // Sea Horse
-        glm::vec3 traslation = glm::vec3(30, 10,-0);
-        movingSeahorse(45, viewer, flatShader, glm::vec3(55 , -40, -20 ) ,27);
-        movingSeahorse(45.1, viewer, flatShader, glm::vec3(55 , -30, 0 ) ,27);
-        movingSeahorse(45.2, viewer, flatShader, glm::vec3(60 , -40, 0 ) ,27);
-        movingSeahorse(45.3, viewer, flatShader, glm::vec3(60 , -30, -20 ) ,27);
-        movingSeahorse(45.4, viewer, flatShader, glm::vec3(65 , -40, -20 ) ,27);
-        movingSeahorse(45.5, viewer, flatShader, glm::vec3(65 , -30, 0 ) ,27);
-        movingSeahorse(45.6, viewer, flatShader, glm::vec3(70 , -40, 0 ) ,27);
-        movingSeahorse(45.7, viewer, flatShader, glm::vec3(70 , -30, -20 ) ,27);
-        movingSeahorse(45.8, viewer, flatShader, glm::vec3(75 , -40, -20 ) ,27);
-        movingSeahorse(45.9, viewer, flatShader, glm::vec3(75 , -30, 0 ) ,27);
+        float time = initialTime;
+        int pos_x = 75;
+        for (int i = 0; i < 5; i++)
+        {
+            if ( i %2 == 0){
+                movingSeahorse(time, viewer, flatShader, glm::vec3(pos_x + 8 , -45, -20 ) ,duration);
+                movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x + 2, -37, -15 ) ,duration);
+                movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x , -30, -5) ,duration);
+                movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x , -23, 0 ) ,duration);
+            }
+            else{
+                movingSeahorse(time, viewer, flatShader, glm::vec3(pos_x  , -45, -0 ) ,duration);
+                movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x , -37, -5 ) ,duration);
+                movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x + 2, -30, -15) ,duration);
+                movingSeahorse(time + 0.1, viewer, flatShader, glm::vec3(pos_x + 8 , -23, -20 ) ,duration);
+            }
+
+            pos_x-=5;
+            time+=0.2;
+    }
+
 
         // Submarine
             // Moving of submarine
-        submarine->addParentTransformKeyframe(GeometricTransformation( glm::vec3(50 , -45, -10 ), glm::angleAxis(-3.14f / 2, glm::vec3(0.0,1.0,0)), 
-                                                            glm::vec3(1,1,1)),25);
-        movingSubmarine( submarine, 45, glm::vec3(50 , -45, -10 ),27);
+        submarine->addParentTransformKeyframe(GeometricTransformation( glm::vec3(52 , -45, -10 ), glm::angleAxis(-3.14f / 2, glm::vec3(0.0,1.0,0)), 
+                                                            glm::vec3(1,1,1)),initialTime-10);
+        movingSubmarine( submarine, initialTime, glm::vec3(52 , -45, -10 ),duration);
             // Moving Aletas    
-        movingAletas (aletas, 45,25);
+        movingAletas (aletas, initialTime,duration);
             //Moving Visor
-        movingVisor(visor,45,25);
+        movingVisor(visor,initialTime,duration);
 }
 
- 
-LightedMeshRenderablePtr* createSubmarine(Viewer& viewer, ShaderProgramPtr flatShader){
-    MaterialPtr silver = Material::Silver();
-    MaterialPtr pearl = Material::Pearl();
-    MaterialPtr emerald = Material::Emerald();
-    MaterialPtr bronze = Material::Bronze();
-    MaterialPtr turquoise = Material::Turquoise();
-    MaterialPtr whiteRubber = Material::WhiteRubber();
-    MaterialPtr yellowRubber = Material::YellowRubber();
-    MaterialPtr gold = Material::Gold();
-    MaterialPtr ruby = Material::Ruby();
-    MaterialPtr green = Material::Green();
-
-
-    LightedMeshRenderablePtr* subamrineGroup= new LightedMeshRenderablePtr[10];
-
-    // Cuerpo
-    subamrineGroup[0] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/cuerpo.obj");
-    subamrineGroup[0]->setMaterial(silver);
-
-    // Aletas
-     subamrineGroup[1] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/aletas.obj");
-    subamrineGroup[1]->setMaterial(ruby);
-
-    // Visor
-     subamrineGroup[2] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/visor.obj");
-    subamrineGroup[2]->setMaterial(bronze);
-
-    // Cola
-     subamrineGroup[3] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/cola.obj");
-    subamrineGroup[3]->setMaterial(yellowRubber);
-
-    // Ventana Lateral
-     subamrineGroup[4] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/ventana_laterales.obj");
-    subamrineGroup[4]->setMaterial(gold);
-
-    // Ventana Principal
-     subamrineGroup[5] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/ventana_principal.obj");
-    subamrineGroup[5]->setMaterial(gold);
-
-    // Ventana visor
-     subamrineGroup[6] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/ventana_visor.obj");
-    subamrineGroup[6]->setMaterial(gold);
-
-    // Vidrio Lateral
-     subamrineGroup[7] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/vidrio_laterales.obj");
-    subamrineGroup[7]->setMaterial(turquoise);
-
-    // Vidio Principal
-     subamrineGroup[8] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/vidrio_principal.obj");
-    subamrineGroup[8]->setMaterial(turquoise);
-
-    // Vidrio visor
-     subamrineGroup[9] = std::make_shared<LightedMeshRenderable>(flatShader, "./../../sfmlGraphicsPipeline/meshes/vidrio_visor.obj");
-    subamrineGroup[9]->setMaterial(turquoise);
-    
-
-    // Children
-    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[1]);
-    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[2]);
-    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[3]);
-    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[4]);
-    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[5]);
-    HierarchicalRenderable :: addChild(subamrineGroup[2], subamrineGroup[6]);
-    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[7]);
-    HierarchicalRenderable :: addChild(subamrineGroup[0], subamrineGroup[8]);
-    HierarchicalRenderable :: addChild(subamrineGroup[2], subamrineGroup[9]);
-
-
-    return subamrineGroup;
-    //movingSubmarine( viewer, submarine);
-    
-}
 
 void movingCamera(Viewer& viewer){
-    if( viewer.getTime() > 45)
+    if( viewer.getTime() > 44)
     {
-        viewer.getCamera().setViewMatrix( glm::lookAt(  glm::vec3( 70, -37, 20 ), 
-                                                        glm::vec3(70, -37, 0), 
+        viewer.getCamera().setViewMatrix( glm::lookAt(  glm::vec3( 90, -35, 25 ), 
+                                                        glm::vec3(90, -35, 0), 
                                                         glm::vec3( 0, 1, 0 ) ) );
     }
-    else if (viewer.getTime() > 21)
+    else if (viewer.getTime() > 19.5)
     {
      //std::cout << viewer.getTime() << std::endl;
-       viewer.getCamera().setViewMatrix( glm::lookAt(   glm::vec3( -60, -40, 20 ), 
-                                                        glm::vec3(-60, -40, 0), 
+       viewer.getCamera().setViewMatrix( glm::lookAt(   glm::vec3( -60, -35, 20 ), 
+                                                        glm::vec3(-60, -35, 0), 
                                                         glm::vec3( 0, 1, 0 ) ) );
     }
     else
@@ -313,8 +334,8 @@ void initialize_scene( Viewer& viewer )
 
     // ------------- Scenes -----------
     Scene1( viewer, submarineGroup[0], submarineGroup[1]);
-    Scene2 ( viewer, phongShader);
-    Scene3 ( viewer,  phongShader, submarineGroup[0],  submarineGroup[1], submarineGroup[2] );
+    Scene2 ( viewer, phongShader, 19.0, 26.0);
+    Scene3 ( viewer,  phongShader, submarineGroup[0],  submarineGroup[1], submarineGroup[2] , 43, 40);
     
 
 //Define a directional light for the whole scene
